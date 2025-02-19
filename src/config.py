65fe60bdd1c7e3f96568d9cb2afaeb09d59f0a60
shrijayan <81805145+shrijayan@users.noreply.github.com>
2025-02-19 22:32:47 +0530

    Refactor ModelConfig attributes to use type annotations for improved clarity and consistency

--- src/config.py
+++ src/config.py
@@ -19,13 +19,13 @@ class ModelConfig:
     early_stopping_patience: int = 50
     eval_and_save_steps: int = 100
     eval_and_save_strategy: str = "steps"
-    load_best_model_at_end=True
-    metric_for_best_model="combined_f1"
-    greater_is_better=True,
+    load_best_model_at_end: bool = True
+    metric_for_best_model: str = "combined_f1"
+    greater_is_better: bool = True
     save_strategy = "best"
     logging_steps: int = 100
     dataset_name: str = "shrijayan/medical_mimic"
-    save_total_limit=2
+    save_total_limit: int = 2
     report_to: str = "wandb"
     output_dir: str = f"/content/drive/MyDrive/Projects/ModernBertTuning/{datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}"
     logging_dir: str = f"{output_dir}/logs"
