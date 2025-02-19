94cec7edd5e893ae4b479f15e61305151f4d6c17
shrijayan <81805145+shrijayan@users.noreply.github.com>
2025-02-19 22:23:33 +0530

    Update ModelConfig parameters for improved training performance

--- src/config.py
+++ src/config.py
@@ -5,20 +5,27 @@ import datetime
 @dataclass
 class ModelConfig:
     model_name: str = "answerdotai/ModernBERT-large"
-    max_length: int = 8192
-    batch_size: int = 2
-    num_epochs: int = 5
+    max_length: int = 512
+    batch_size: int = 16
+    num_epochs: int = 10
     seed: int = 42
     classification_type: str = "multi_label_classification"
     gradient_accumulation_steps: int = 1
     warmup_ratio: float = 0.1
     weight_decay: float = 0.01
-    learning_rate: float = 2e-5
+    learning_rate: float = 5e-5
+    max_grad_norm: float = 1.0
     fp16: bool = True
     early_stopping_patience: int = 50
-    eval_and_save_steps: int = 500
+    eval_and_save_steps: int = 100
     eval_and_save_strategy: str = "steps"
-    logging_steps: int = 10
+    load_best_model_at_end=True
+    metric_for_best_model="combined_f1"
+    greater_is_better=True,
+    save_strategy = "best"
+    logging_steps: int = 100
     dataset_name: str = "shrijayan/medical_mimic"
+    save_total_limit=3
     report_to: str = "wandb"
     output_dir: str = f"/content/drive/MyDrive/Projects/ModernBertTuning/{datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}"
+    logging_dir: str = f"{output_dir}/logs"
